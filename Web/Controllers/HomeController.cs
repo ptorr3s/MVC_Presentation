cda1117aad31ab4c96eb14a4dba89bc32025e591
mwoodruff <mwoodruff@klick.com>
2013-02-26 04:20:14 -0500

    ControllerModelList

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using MVC_Presentation.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace Web.Controllers {
 	public class HomeController : Controller {
@@ -6,7 +8,12 @@ namespace Web.Controllers {
 		//
 		// GET: /Home/
 		public ActionResult Index() {
-			return View();
+			return View(
+				new List<Task>() {
+                new Task() { id = 1, isComplete = true, title = "Do Groceries" },
+                new Task() { id = 2, isComplete = true, title = "Pick up dry cleaning" },
+                new Task() { id = 3, isComplete = false, title = "Fix Jeff Z's code" }
+            });
 		}
 	}
 }
---
+++ Web/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Presentation.Models {
+	public class Task {
+		public int id { get; set; }
+		public bool isComplete { get; set; }
+		[Required]
+		public string title { get; set; }
+	}
+}

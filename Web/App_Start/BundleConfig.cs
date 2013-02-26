609f6dfa2c14d6c37d0e3a7c14fcd0e08dadb387
mwoodruff <mwoodruff@klick.com>
2013-02-26 03:33:30 -0500

    BasicTaskList

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -8,6 +8,10 @@ namespace Web {
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 							"~/Scripts/jquery-{version}.js"));
 
+			bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+				"~/Scripts/knockout-*",
+				"~/Scripts/knockout.mapping-*"));
+
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 			bundles.Add(new ScriptBundle("~/Content/js").Include("~/Content/Site.js"));
 		}

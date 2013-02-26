7ff0ee321260a1e70a183177db9d7dcfbec2ea62
mwoodruff <mwoodruff@klick.com>
2013-02-26 01:52:03 -0500

    BasicSite

---
+++ Web/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Web {
+	public class BundleConfig {
+		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+		public static void RegisterBundles(BundleCollection bundles) {
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+							"~/Scripts/jquery-{version}.js"));
+
+			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+			bundles.Add(new ScriptBundle("~/Content/js").Include("~/Content/Site.js"));
+		}
+	}
+}
